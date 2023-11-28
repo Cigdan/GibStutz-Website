96c4861012e86c45376c440aaa1f786b250d34ad
Cigdan <72510831+Cigdan@users.noreply.github.com>
2023-11-28 19:57:24 +0100

    added WIP Sidebar for phones

---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import layout from "./layout.module.scss"
+import RefsContext from "../context/RefsContext";
+import { scrollToSection } from "../functions/routing";
+import logo from "../assets/images/logo.png";
+
+type SidebarProps = {
+  isSidebarOpen: boolean;
+  setIsSidebaropen: (isSidebarOpen: boolean) => void;
+}
+
+export default function Sidebar(props : SidebarProps) {
+  function sidebarScroll(target: string) {
+    scrollToSection(target, refs, setCurrentSection)
+    props.setIsSidebaropen(false)
+  }
+  const { setCurrentSection, refs } = useContext(RefsContext);
+  return (
+    <div onClick={() => props.setIsSidebaropen(false)} className={props.isSidebarOpen ?  layout.sidebarContainer : ""}>
+        <div onClick={(e) => e.stopPropagation()} className={`${layout.sideBar} ${props.isSidebarOpen ? layout.sidebarOpen : layout.sidebarClosed}`}>
+        <img
+          onClick={() => scrollToSection("home", refs, setCurrentSection)}
+          src={logo}
+          width="100px"
+          height="100px"
+          alt="GibStutz Logo"
+        />
+            <ul className={layout.sidebarNav}>
+                <li onClick={() => sidebarScroll("home")}>Home</li>
+                <li onClick={() => sidebarScroll("stats")}>Stats</li>
+                <li onClick={() => sidebarScroll("team")} >Team</li>
+                <li onClick={() => sidebarScroll("home")} >Route #4</li>
+            </ul>
+        </div>
+    </div>
+
+  )
+}
