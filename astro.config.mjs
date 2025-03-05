2c534923e1b701b60329b257a1de96f11a1f1cc1
Cigdan <colinwalker063@gmail.com>
2025-03-05 11:23:10 +0100

    added countdown to stat cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), alpinejs()]
 });
